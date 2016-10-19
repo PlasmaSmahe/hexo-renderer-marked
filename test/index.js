ac7695894da6e3a95a969beb1a104698b5f157b1
ninjuki <ninjuki@gmail.com>
2016-10-19 17:24:18 +0200

    Mise à jours en version 0.2.11

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var should = require('chai').should();
+var should = require('chai').should(); // eslint-disable-line
 var util = require('hexo-util');
 
-describe('Marked renderer', function(){
+describe('Marked renderer', function() {
   var ctx = {
     config: {
       marked: {}
@@ -12,7 +12,7 @@ describe('Marked renderer', function(){
 
   var r = require('../lib/renderer').bind(ctx);
 
-  it('default', function(){
+  it('default', function() {
     var code = 'console.log("Hello world");';
 
     var body = [
@@ -30,10 +30,32 @@ describe('Marked renderer', function(){
     var result = r({text: body});
 
     result.should.eql([
-      '<h1 id="Hello_world">Hello world</h1>',
-      '<pre><code>' + util.highlight(code, {gutter: false, wrap: false}) + '\n</code></pre>',
-      '<h2 id="Hello_world-1">Hello world</h2>',
-      '<p>hello</p>'
-    ].join('') + '\n');
+        '<h1 id="Hello-world"><a href="#Hello-world" class="headerlink" title="Hello world"></a>Hello world</h1>',
+        '<pre><code>' + util.highlight(code, {gutter: false, wrap: false}) + '\n</code></pre>',
+        '<h2 id="Hello-world-1"><a href="#Hello-world-1" class="headerlink" title="Hello world"></a>Hello world</h2>',
+        '<p>hello</p>'
+      ].join('') + '\n');
+  });
+
+  it('should render headings with links', function() {
+    var body = [
+      '## [hexo-server]',
+      '',
+      '[hexo-server]: https://github.com/hexojs/hexo-server'
+    ].join('\n');
+
+    var result = r({text: body});
+
+    result.should.eql([
+      '<h2 id="hexo-server"><a href="#hexo-server" class="headerlink" title="hexo-server"></a>',
+      '<a href="https://github.com/hexojs/hexo-server">hexo-server</a></h2>'
+    ].join(''));
+  });
+
+  it('should handle chinese headers properly', function() {
+    var body = '# 中文';
+    var result = r({text: body});
+
+    result.should.eql('<h1 id="中文"><a href="#中文" class="headerlink" title="中文"></a>中文</h1>');
   });
 });
